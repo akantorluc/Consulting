fb12cc71edada7a0f56ac5bcc45afed894d3e62c
BradHughes91 <brad_r_hughes@hotmail.com>
2019-09-19 12:11:25 -0500

    changes

--- 010_data_manipulations.R
+++ 010_data_manipulations.R
@@ -1,6 +1,12 @@
 library(dplyr)
 
-#create variables combining otehr plants that are not Typha and split trmt variable into dummies
+
+#create variables combining other plants that are not Typha and split trmt variable into dummies
 Consult = Consult %>% 
           mutate(nonTypha = potber + utrmin + utrvul, trmt_above = ifelse(trmt == 'Above', 1, 0),
                  trmt_below = ifelse(trmt == 'Below', 1, 0), trmt_Mow = ifelse(trmt == 'Mow', 1, 0))
+
+
+#high variance issues with water depth and typha so we 
+#divided it by 10 to be at teh same scal as teh otehr variables
+Consult = Consult %>% mutate(Typha = Typha/10, waterDepth = waterDepth/10)
--- 020_SEM.R
+++ 020_SEM.R
@@ -1,7 +1,28 @@
 library(lavaan)
 
 mod.1 = 'waterDepth ~ year
-        vegCover ~ waterDepth
-        vegCover ~ year'
+        Typha ~ waterDepth + trmt_above + trmt_below + trmt_Mow
+        nonTypha ~ Typha
+        Typha ~ year'
 mod = sem(mod.1, data = Consult)
 summary(mod)
+
+
+mod.2 = 'waterDepth ~ year
+        Typha ~ waterDepth + trmt_above + trmt_below + trmt_Mow
+        Typha ~ nonTypha
+        Typha ~ year'
+mod2 = sem(mod.2, data = Consult)
+summary(mod2)
+
+
+mod.3 = 'waterDepth ~ year
+        Typha ~ waterDepth + trmt_above + trmt_below + trmt_Mow
+        Typha ~ potber + utrmin + utrvul
+        Typha ~ year
+        potber ~ Typha
+        utrmin ~ Typha
+        utrvul ~ Typha'
+mod3 = sem(mod.3, data = Consult)
+#water depth and Typha coefficients should be multiplied by 10
+summary(mod3)
