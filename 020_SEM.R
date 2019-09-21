9946e3b2903bc01c09366caf34a3d01d022ca3fb
akantorluc <akantor@luc.edu>
2019-09-21 15:06:40 -0500

    more models

--- 020_SEM.R
+++ 020_SEM.R
@@ -26,3 +26,11 @@ mod.3 = 'waterDepth ~ year
 mod3 = sem(mod.3, data = Consult)
 #water depth and Typha coefficients should be multiplied by 10
 summary(mod3)
+
+mod.4 = 'waterDepth ~ year
+        Typha ~ waterDepth + trmt_above + trmt_below + trmt_Mow
+        Typha ~ nonTypha
+        Typha ~ year
+        nonTypha ~ year + waterDepth + trmt_above + trmt_below + trmt_Mow'
+mod4 = sem(mod.4, data = Consult)
+summary(mod4)
