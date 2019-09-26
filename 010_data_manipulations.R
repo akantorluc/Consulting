278d810d377ca47fbb3bc04b83663ee1eff31d9e
BradHughes91 <brad_r_hughes@hotmail.com>
2019-09-26 10:29:50 -0500

    new data frame has trmt_control

--- 010_data_manipulations.R
+++ 010_data_manipulations.R
@@ -4,9 +4,12 @@ library(dplyr)
 #create variables combining other plants that are not Typha and split trmt variable into dummies
 Consult = Consult %>% 
           mutate(nonTypha = potber + utrmin + utrvul, trmt_above = ifelse(trmt == 'Above', 1, 0),
-                 trmt_below = ifelse(trmt == 'Below', 1, 0), trmt_Mow = ifelse(trmt == 'Mow', 1, 0))
+                 trmt_below = ifelse(trmt == 'Below', 1, 0), trmt_Mow = ifelse(trmt == 'Mow', 1, 0),
+                 trmt_control = ifelse(trmt == 'Control', 1, 0))
 
 
 #high variance issues with water depth and typha so we 
 #divided it by 10 to be at teh same scal as teh otehr variables
 Consult = Consult %>% mutate(Typha = Typha/10, waterDepth = waterDepth/10)
+
+save(Consult, file="Consult_data.Rdata")
