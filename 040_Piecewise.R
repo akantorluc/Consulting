ec283d7ff107e07300cfda30d954a409fa97aba3
akantorluc <akantor@luc.edu>
2019-09-29 13:00:52 -0500

    new

--- 040_Piecewise.R
+++ 040_Piecewise.R
@@ -7,13 +7,13 @@ library(nlme)
 # We have two hierarchies. Between plot and years within plot
 Consult.modlist <- list(
   lme(waterDepth ~ year, random = ~1|plot, data = Consult), 
-  lme(Typha ~ waterDepth + trmt, random = ~1|plot, data = Consult), 
+  lme(Typha ~ waterDepth + trmt_yes, random = ~1|plot, data = Consult), 
   glmer(potber ~ waterDepth + Typha + (1|plot), family = poisson(link = "log"), data = Consult))
 sem.fit(Consult.modlist,Consult)
 
 Consult.modlist <- psem(
   lme(waterDepth ~ year, random = ~1|plot, data = Consult), 
-  lme(Typha ~ waterDepth + trmt, random = ~1|plot, data = Consult), 
+  lme(Typha ~ waterDepth + trmt_yes, random = ~1|plot, data = Consult), 
   glmer(potber ~ waterDepth + Typha + (1|plot), family = poisson(link = "log"), data = Consult))
 summary(Consult.modlist)
 
