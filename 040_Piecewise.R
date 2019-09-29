b857023ad1bf378117ef686bde10b7f0bb468f2f
akantorluc <akantor@luc.edu>
2019-09-29 09:57:28 -0500

    Piecewise

---
+++ 040_Piecewise.R
@@ -0,0 +1,18 @@
+library(devtools)
+library(piecewiseSEM)
+library(lmerTest)
+library(nlme)
+
+# piecewise SEM model without plots in it
+# We have two hierarchies. Between plot and years within plot
+Consult.modlist <- list(
+  lme(waterDepth ~ year, random = ~1|plot, data = Consult), 
+  lme(Typha ~ waterDepth + trmt, random = ~1|plot, data = Consult), 
+  glmer(potber ~ waterDepth + Typha + (1|plot), family = poisson(link = "log"), data = Consult))
+sem.fit(Consult.modlist,Consult)
+
+Consult.modlist <- psem(
+  lme(waterDepth ~ year, random = ~1|plot, data = Consult), 
+  lme(Typha ~ waterDepth + trmt, random = ~1|plot, data = Consult), 
+  glmer(potber ~ waterDepth + Typha + (1|plot), family = poisson(link = "log"), data = Consult))
+summary(Consult.modlist)
