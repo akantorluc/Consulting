6a0db3f4a31b9283acaad260be841db7bce09a35
akantorluc <akantor@luc.edu>
2019-11-03 15:29:45 -0600

    new

--- 040_Piecewise.R
+++ 040_Piecewise.R
@@ -13,16 +13,6 @@ Consult.modlist <- list(
   )
 summary(Consult.modlist,Consult)
 
-Consult.modlist <- psem(
-  lmer(waterDepth ~ year + (1|plot), data = Consult_hurdle),
-  glmer(Typha_gtzero ~ waterDepth + trmt_yes + (1|plot), family = binomial(link=logit), data=Consult_hurdle),
-  glmer(Typha_dec ~ waterDepth + trmt_yes + (1|plot), 
-      family = Gamma(link=log), data = subset(Consult_hurdle, Typha_gtzero == 1)),
-  glm(potber_gtzero ~ 1, family = binomial(link=logit), data=Consult_hurdle),
-  glmer(potber_dec ~ waterDepth + Typha_dec + (1|plot), 
-      family = Gamma(link=log), data = subset(Consult_hurdle, potber_gtzero == 1))
-  )
-summary(Consult.modlist)
 
 #try psem for binomials and then psem for gamma stuff
 Consult.modlist <- psem(
@@ -63,3 +53,54 @@ summary(Consult.modlist, Consult_hurdle)
 # Piecewise links
 # https://jonlefcheck.net/2014/07/06/piecewise-structural-equation-modeling-in-ecological-research/
 # https://esajournals.onlinelibrary.wiley.com/doi/full/10.1890/08-1034.1
+
+model1 <- 'potber13 ~ potber12 + Typha12 + waterDepth12
+          Typha13 ~ Typha12 + waterDepth12 + trmt_yes
+          potber12 ~ potber11 + Typha11 + waterDepth11
+          Typha12 ~ Typha11 + waterDepth11 + trmt_yes
+          potber13 ~~ Typha13
+          potber12 ~~ Typha12'
+modfit <- sem(model1, data = Consult_yearly, estimator = "mlm")
+summary(modfit) # .298
+
+model2 <- 'potber13 ~ potber12 + utrmin12 + utrvul12 + Typha12 + waterDepth12
+          utrmin13 ~ potber12 + utrmin12 + utrvul12 + Typha12 + waterDepth12
+          utrvul13 ~ potber12 + utrmin12 + utrvul12 + Typha12 + waterDepth12
+          Typha13 ~ Typha12 + potber12 + utrmin12 + utrvul12 + waterDepth12 + trmt_yes
+          potber12 ~ potber11 + utrmin11 + utrvul11 + Typha11 + waterDepth11
+          utrmin12 ~ potber11 + utrmin11 + utrvul11 + Typha11 + waterDepth11
+          utrvul12 ~ potber11 + utrmin11 + utrvul11 + Typha11 + waterDepth11
+          Typha12 ~ Typha11 + waterDepth11 + trmt_yes
+          potber13 ~~ Typha13
+          potber12 ~~ Typha12
+          utrmin13 ~~ Typha13
+          utrmin12 ~~ Typha12
+          utrvul13 ~~ Typha13
+          utrvul12 ~~ Typha12
+          potber13 ~~ utrmin13
+          potber12 ~~ utrmin12
+          potber13 ~~ utrvul13
+          potber12 ~~ utrvul12
+          utrmin13 ~~ utrvul13
+          utrmin12 ~~ utrvul12'
+modfit <- sem(model2, data = Consult_yearly, estimator = "mlm")
+summary(modfit) 
+
+model3 <- 'Typha13 ~ Typha12 + waterDepth12 + trmt_yes + site
+          Typha12 ~ Typha11 + waterDepth11 + trmt_yes + site'
+modfit <- sem(model3, data = Consult_yearly, estimator = "mlm")
+summary(modfit) # .437
+
+model4 <- 'Typha13 ~ Typha12 + waterDepth12 + trmt_yes
+          Typha12 ~ Typha11 + waterDepth11 + trmt_yes
+          vegCover13 ~ Typha12 + vegCover12 + waterDepth12 + trmt_yes
+          vegCover12 ~ Typha11 + vegCover11 + waterDepth11 + trmt_yes
+          vegCover13 ~~ Typha13
+          vegCover12 ~~ Typha12'
+modfit <- sem(model4, data = Consult_yearly, estimator = "mlm")
+summary(modfit) 
+
+model5 <- 'Typha13 ~ Typha12
+          Typha12 ~ Typha11 + trmt_yes'
+modfit <- sem(model5, data = Consult_yearly, estimator = "mlm")
+summary(modfit) # .779
